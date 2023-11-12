a3dd85b75c61a88d939496f5aa43bc3b5ecb8a60
karitkeya <kartikeyasharma2006@gmail.com>
2023-11-12 13:14:17 +0530

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/Prof-Software", "_blank")
+            window.open("https://github.com/Prof-Software/text-summarizer", "_blank")
           }
           className='black_btn'
         >
